b613f0d292203545dd8474dca5d77500e007b845
Marcos Henrique <marcoshenriqueh393@gmail.com>
2024-01-04 09:50:20 -0300

    finalizando api

---
+++ app.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import TelegramBot from 'node-telegram-bot-api';
+import express from 'express';
+
+import {format} from 'date-fns';
+
+const token = process.env.TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+const app = express();
+const port = 3001;
+
+app.post('/webhook', async (req, res) => {
+  const chatId = process.env.CHAT_ID;
+  const messageText = `Alerta! 💣 Movimento detectado - ${format(new Date(), 'dd/MM/yyyy \'às\' HH:mm')}`;
+
+  try {
+    await bot.sendMessage(chatId, messageText);
+    console.log('Mensagem enviada com sucesso!');
+  } catch (error) {
+    console.error('Erro ao enviar mensagem:', error.message);
+  }
+
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
